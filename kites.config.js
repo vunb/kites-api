f741eeefda25d2538b44e8010ceb187d17f5f195
Vunb <nhubaovu@gmail.com>
2018-07-08 10:37:21 +0700

    init data source manager

--- kites.config.js
+++ kites.config.js
@@ -5,6 +5,29 @@ module.exports = {
         actions: true,
         crud: true,
         apiDirectory: 'api',
-        apiPrefix: '/api'
+        apiPrefix: '/api',
+        connection: 'memory', // default connection
+        dataSource: [{
+                name: 'mySQL',
+                driver: 'mysql', // or mariadb
+                host: 'localhost',
+                port: '3306',
+                username: 'test',
+                password: 'test',
+                database: 'test',
+                active: false, // auto connect to support multiple connections, default: false
+                pool: true // optional for use pool directly
+            },
+            {
+                name: 'mongodb',
+                driver: 'mongoose', // or mongoose
+                host: 'localhost',
+                port: '27101',
+                username: 'test',
+                password: 'test',
+                database: 'test',
+                rs: false // replication set
+            }
+        ]
     }
 }
